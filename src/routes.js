b7f5d1a8b51bb7c6e5ef9391d61fdb3398d27268
Vicente <vcmunoz@uc.cl>
2018-11-01 23:54:55 -0300

    Add session routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ const hello = require('./routes/hello');
 const index = require('./routes/index');
 const messages = require('./routes/messages');
 const groups = require('./routes/groups');
+const session = require('./routes/session');
 
 
 const router = new KoaRouter();
@@ -12,5 +13,6 @@ router.use('/', index.routes());
 router.use('/hello', hello.routes());
 router.use('/messages', messages.routes());
 router.use('/groups', groups.routes());
+router.use('/session', session.routes());
 
 module.exports = router;

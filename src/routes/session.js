41e1cc4ccb50efad8e2accb21da0361746fc64db
administrator <administrator@charette12.ing.puc.cl>
2018-11-02 23:27:48 -0300

    :construction: Established base profile and session structure

---
+++ src/routes/session.js
@@ -0,0 +1,9 @@
+const KoaRouter = require('koa-router');
+const queryEngine  = require('../lib/queryEngine.js');
+
+const router = new KoaRouter();
+
+const API_URL = 'http://charette11.ing.puc.cl';
+
+module.exports = router;
+

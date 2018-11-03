125064309ed172e78ba6fa3253106f49695a2f50
Vicente <vcmunoz@uc.cl>
2018-11-03 02:41:40 -0300

    add signup route

--- src/routes/session.js
+++ src/routes/session.js
@@ -6,14 +6,16 @@ const { queryEngine } = require('../lib/queryEngine.js');
 // message para reportar los errores si los hay.
 
 router.get('session-new', '/', async (ctx) => {
+  console.log("Entramos al primer middleware. Ahora rendereamos.");
   ctx.render('session/new',
             {
               msg: '',
-              submitPath: ctx.router.url('session-create'),
+              submitLoginPath: ctx.router.url('session-create-login'),
+              submitSignupPath: ctx.router.url('session-create-signup'),
             },)
 });
 
-router.put('session-create', '/', async (ctx) => {
+router.put('session-create-login', '/login', async (ctx) => {
   const { username, password } = ctx.request.body;
   const response = await queryEngine.loginAPI(API_URL, username, password);
   const user = response.user;
@@ -30,14 +32,42 @@ router.put('session-create', '/', async (ctx) => {
   } else {
     await ctx.render('session/new', {
       msg: 'nombre de usuario o contraseña incorrectos',
-      submitPath: ctx.router.url('session-create'),
+      submitLoginPath: ctx.router.url('session-create-login'),
+      submitSignupPath: ctx.router.url('session-create-signup'),
+    });
+  }
+});
+
+router.put('session-create-signup', '/signup', async (ctx) => {
+  const {
+    username,
+    first_name,
+    last_name,
+    email,
+    password } = ctx.request.body;
+  const response = await queryEngine.signUpAPI((API_URL, username, first_name, last_name, email, password));
+  if (response.status_code === 201) {
+    const user = response.user;
+    const userkey = await ctx.orm.UserKey.build({
+      'userId': user.id,
+      'token': user.oauth_token,
+    })
+    await userkey.save();
+    ctx.session.currentUserId = user.id;
+    ctx.flashMessage.notice = 'Inicio de sesión exitoso';
+    ctx.redirect('/');
+  } else {
+    await ctx.render('session/new', {
+      msg: 'El registro falló. Prueba utilizando otro nombre de usuario u otro correo.',
+      submitLoginPath: ctx.router.url('session-create-login'),
+      submitSignupPath: ctx.router.url('session-create-signup'),
     });
   }
 });
 
 router.delete('session-destroy', '/', async (ctx) => {
-  ctx.orm.UserKey.findOne( { where: { 'userId': ctx.session.currentUserId } }
-  ).then(userkey => await userkey.destroy());
+  const userkey = ctx.orm.UserKey.findOne( { where: { 'userId': ctx.session.currentUserId } } );
+  await userkey.destroy();
   delete ctx.session.currentUserId;
   ctx.flashMessage.notice = 'Término de sesión exitoso';
   ctx.redirect('/');

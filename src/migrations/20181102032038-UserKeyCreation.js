c718975c3b87a8f326edf92706729a2b56f2c556
Vicente <vcmunoz@uc.cl>
2018-11-03 04:45:12 -0300

    Add columns and rename table

--- src/migrations/20181102032038-UserKeyCreation.js
+++ src/migrations/20181102032038-UserKeyCreation.js
@@ -1,19 +1,32 @@
 module.exports = {
   up(queryInterface, Sequelize) {
-	
 
- return queryInterface.createTable('users', {
+
+ return queryInterface.createTable('UserKeys', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
       token: {
         allowNull: false,
         type: Sequelize.STRING,
       },
       userId: {
-	primaryKey: true,
         allowNull: false,
         type: Sequelize.STRING,
         unique: true,
         notEmpty: true,
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
     });
   },
 

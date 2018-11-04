0afce42f868a0377e6fe5e451712b1d8e08e4d27
mapuentes <mapuentes@uc.cl>
2018-11-04 14:26:07 -0300

    views login-signup ready

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,7 +2,6 @@ const KoaRouter = require('koa-router');
 const router = new KoaRouter();
 
 router.get('users', '/', async (ctx) => {
-  console.log("xd")
   await ctx.render('users/index', {
     notice: ctx.flashMessage.notice,
   });
@@ -12,9 +11,8 @@ router.post('userscreate', '/', async (ctx) => {
     const user = ctx.orm.User.build(ctx.request.body);
     user.userName = ctx.request.body.fields.userName;
     user.password = ctx.request.body.fields.password;
-    user.email = ctx.request.body.fields.email;
     try {
-      await user.save({ fields: ['userName', 'password', 'email'] });
+      await user.save({ fields: ['userName', 'password'] });
       ctx.flashMessage.notice = 'Usuario creado con exito';
       ctx.session.userId = user.id;
       //return ctx.redirect(ctx.router.url('profile'));

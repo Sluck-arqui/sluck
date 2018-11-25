b28805bc2162489b504861a934558cc324f012cc
mapuentes <mapuentes@uc.cl>
2018-11-25 12:13:50 -0300

    some changes

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 const config = {
   default: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: 'mauricio',
+    password: 'Candita5.',
     dialect: process.env.DB_DIALECT || 'postgres',
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST || 'charette10.ing.puc.cl',
+    database: 'mauriciodb',
+    host: process.env.DB_HOST || '127.0.0.1',
     operatorsAliases: false,
   },
   development: {
